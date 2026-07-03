c502f61b738777ed5b305855e42815c16e3a6e48
zzh <elliot_zh@proton.me>
2026-07-03 16:36:54 +0800

    feat: support operands in different types when constructing the graph, update op_test

--- src/deep/graph.py
+++ src/deep/graph.py
@@ -11,13 +11,19 @@ class Op(ABC):
 
 
 class Add(Op):
-    def __init__(self, a1: Tensor, a2: Tensor, sub=False):
-        self.a1, self.a2 = a1, a2
+    def __init__(self, a1, a2, sub=False):
+        self.a1_np, self.a2_np = (
+            i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
+        )
+        self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
         self.sub = sub
 
     def grad(self, grad):
         # FIXME: handle operands that is not Tensor
-        self.a1.backward(grad)
+        if self.a1 is not None:
+            self.a1.backward(grad)
+        if self.a2 is None:
+            return
         if self.sub:
             self.a2.backward(-1 * grad)
         else:
@@ -25,34 +31,51 @@ class Add(Op):
 
 
 class Mul(Op):
-    def __init__(self, a1: Tensor, a2: Tensor):
-        self.a1, self.a2 = a1, a2
+    def __init__(self, a1, a2):
+        self.a1_np, self.a2_np = (
+            i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
+        )
+        self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
 
     def grad(self, grad):
-        self.a1.backward(grad * self.a2.to_np())
-        self.a2.backward(self.a1.to_np() * grad)
+        if self.a1 is not None:
+            self.a1.backward(grad * self.a2_np())
+        if self.a2 is not None:
+            self.a2.backward(self.a1_np() * grad)
 
 
 class Div(Op):
-    def __init__(self, a1: Tensor, a2: Tensor):
-        self.a1, self.a2 = a1, a2
+    def __init__(self, a1, a2):
+        self.a1_np, self.a2_np = (
+            i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
+        )
+        self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
 
     def grad(self, grad):
-        self.a1.backward(grad * self.a2.to_np())
-        self.a2.backward(-1 * self.a1.to_np() / (self.a2.to_np() ** 2) * grad)
+        if self.a1 is not None:
+            self.a1.backward(grad * self.a2.to_np())
+        if self.a2 is not None:
+            self.a2.backward(-1 * self.a1.to_np() / (self.a2.to_np() ** 2) * grad)
 
 
 class MatMul(Op):
-    def __init__(self, a1: Tensor, a2: Tensor):
-        self.a1, self.a2 = a1, a2
+    def __init__(self, a1, a2):
+        self.a1_np, self.a2_np = (
+            i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
+        )
+        self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
 
     def grad(self, grad):
-        self.a1.backward(grad @ self.a2.to_np().T)
-        self.a2.backward(self.a1.to_np().T @ grad)
+        if self.a1 is not None:
+            self.a1.backward(grad @ self.a2.to_np().T)
+        if self.a2 is not None:
+            self.a2.backward(self.a1.to_np().T @ grad)
 
 
 class Exp(Op):
-    def __init__(self, input: Tensor):
+    def __init__(
+        self, input: Tensor
+    ):  # node of exp is introduced only when the input is a Tensor
         self.input = input
 
     def grad(self, grad):
@@ -60,7 +83,9 @@ class Exp(Op):
 
 
 class Log(Op):
-    def __init__(self, input: Tensor):
+    def __init__(
+        self, input: Tensor
+    ):  # node of log is introduced only when the input is a Tensor
         self.input = input
 
     def grad(self, grad):
--- tests/op_test.py
+++ tests/op_test.py
@@ -6,46 +6,86 @@ from deep import *
 # TODO: test more types and shapes (including broadcasting)
 
 
+def try_ufunc_call(ufunc, *args_np):
+    args = [Tensor(i) if isinstance(i, np.ndarray) else i for i in args_np]
+    res = ufunc(*args)
+    assert (res.to_np() == ufunc(*args_np)).all()
+
+
 def test_add():
+    # general add
     a1_np, a2_np = np.random.rand(3, 4), np.random.rand(3, 4)
-    a1, a2 = Tensor(a1_np), Tensor(a2_np)
-    res = a1 + a2
-    assert (res.to_np() == a1_np + a2_np).all()
+    try_ufunc_call(np.add, a1_np, a2_np)
+
+    # add with broadcast
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(4)
+    try_ufunc_call(np.add, a1_np, a2_np)
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(3, 1)
+    try_ufunc_call(np.add, a1_np, a2_np)
+
+    # add with scalar
+    a1_np, a2_np = np.random.rand(3, 4), 5
+    try_ufunc_call(np.add, a1_np, a2_np)
 
 
 def test_sub():
+    # general sub
     a1_np, a2_np = np.random.rand(3, 4), np.random.rand(3, 4)
-    a1, a2 = Tensor(a1_np), Tensor(a2_np)
-    res = a1 - a2
-    assert (res.to_np() == a1_np - a2_np).all()
+    try_ufunc_call(np.subtract, a1_np, a2_np)
+
+    # sub with broadcast
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(4)
+    try_ufunc_call(np.subtract, a1_np, a2_np)
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(3, 1)
+    try_ufunc_call(np.subtract, a1_np, a2_np)
+
+    # sub with scalar
+    a1_np, a2_np = np.random.rand(3, 4), 5
+    try_ufunc_call(np.subtract, a1_np, a2_np)
 
 
 def test_mul():
+    # general mul
     a1_np, a2_np = np.random.rand(3, 4), np.random.rand(3, 4)
-    a1, a2 = Tensor(a1_np), Tensor(a2_np)
-    res = a1 * a2
-    assert (res.to_np() == a1_np * a2_np).all()
+    try_ufunc_call(np.multiply, a1_np, a2_np)
 
-
-def test_mul_broadcast():
+    # mul with broadcast
     a1_np, a2_np = np.random.rand(3, 4), np.random.rand(4)
-    a1, a2 = Tensor(a1_np), Tensor(a2_np)
-    res = a1 * a2
-    assert (res.to_np() == a1_np * a2_np).all()
+    try_ufunc_call(np.multiply, a1_np, a2_np)
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(3, 1)
+    try_ufunc_call(np.multiply, a1_np, a2_np)
+
+    # mul with scalar
+    a1_np, a2_np = np.random.rand(3, 4), 5
+    try_ufunc_call(np.multiply, a1_np, a2_np)
 
 
 def test_matmul():
+    # general matmul
     a1_np, a2_np = np.random.rand(3, 4), np.random.rand(4, 5)
-    a1, a2 = Tensor(a1_np), Tensor(a2_np)
-    res = a1 @ a2
-    assert (res.to_np() == a1_np @ a2_np).all()
+    try_ufunc_call(np.matmul, a1_np, a2_np)
+
+    # matmul with broadcast
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(4)
+    try_ufunc_call(np.matmul, a1_np, a2_np)
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(4, 1)
+    try_ufunc_call(np.matmul, a1_np, a2_np)
 
 
 def test_div():
+    # general div
     a1_np, a2_np = np.random.rand(3, 4), np.random.rand(3, 4)
-    a1, a2 = Tensor(a1_np), Tensor(a2_np)
-    res = a1 / a2
-    assert (res.to_np() == a1_np / a2_np).all()
+    try_ufunc_call(np.divide, a1_np, a2_np)
+
+    # div with broadcast
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(4)
+    try_ufunc_call(np.divide, a1_np, a2_np)
+    a1_np, a2_np = np.random.rand(3, 4), np.random.rand(3, 1)
+    try_ufunc_call(np.divide, a1_np, a2_np)
+
+    # div with scalar
+    a1_np, a2_np = np.random.rand(3, 4), 5
+    try_ufunc_call(np.divide, a1_np, a2_np)
 
 
 def test_pow():
