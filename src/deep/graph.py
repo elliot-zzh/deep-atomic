fix: refactor graph node code, fix incorrect compute graph calc via topo sorting, fix softmax / log_softmax behavior

src/deep/graph.py
@@ -12,12 +12,27 @@ class Op(ABC):
         pass
 
 
-class Add(Op):
-    def __init__(self, a1, a2, sub=False):
+class SingleOp(Op):
+    def __init__(self, input: Tensor):
+        self.input = input
+        self.input.depended_count[0] += 1
+
+
+class TwoOp(Op):
+    def __init__(self, a1, a2):
         self.a1_np, self.a2_np = (
             i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
         )
         self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
+        if isinstance(self.a1, Tensor):
+            self.a1.depended_count[0] += 1
+        if isinstance(self.a2, Tensor):
+            self.a2.depended_count[0] += 1
+
+
+class Add(TwoOp):
+    def __init__(self, a1, a2, sub=False):
+        super().__init__(a1, a2)
         self.sub = sub
 
     def backward(self, grad):
@@ -30,12 +45,9 @@ class Add(Op):
         self.a2.backward(grad)
 
 
-class Mul(Op):
+class Mul(TwoOp):
     def __init__(self, a1, a2):
-        self.a1_np, self.a2_np = (
-            i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
-        )
-        self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
+        super().__init__(a1, a2)
 
     def backward(self, grad):
         if self.a1 is not None:
@@ -44,60 +56,51 @@ class Mul(Op):
             self.a2.backward(self.a1_np * grad)
 
 
-class Div(Op):
+class Div(TwoOp):
     def __init__(self, a1, a2):
-        self.a1_np, self.a2_np = (
-            i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
-        )
-        self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
+        super().__init__(a1, a2)
 
     def backward(self, grad):
         if self.a1 is not None:
             self.a1.backward(grad / self.a2_np)
         if self.a2 is not None:
-            self.a2.backward(-1 * self.a1.to_np() / (self.a2.to_np() ** 2) * grad)
+            self.a2.backward(-1 * self.a1_np / (self.a2_np**2) * grad)
 
 
-class MatMul(Op):
+class MatMul(TwoOp):
     def __init__(self, a1, a2):
-        self.a1_np, self.a2_np = (
-            i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
-        )
-        self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
+        super().__init__(a1, a2)
 
     def backward(self, grad):
         if self.a1 is not None:
-            self.a1.backward(grad @ self.a2.to_np().swapaxes(-1, -2))
+            self.a1.backward(grad @ self.a2_np.swapaxes(-1, -2))
         if self.a2 is not None:
-            self.a2.backward(self.a1.to_np().swapaxes(-1, -2) @ grad)
+            self.a2.backward(self.a1_np.swapaxes(-1, -2) @ grad)
 
 
-class Exp(Op):
+class Exp(SingleOp):
     def __init__(
         self, input: Tensor
     ):  # node of exp is introduced only when the input is a Tensor
-        self.input = input
+        super().__init__(input)
 
     def backward(self, grad):
         self.input.backward(np.exp(self.input.to_np()) * grad)
 
 
-class Log(Op):
+class Log(SingleOp):
     def __init__(
         self, input: Tensor
     ):  # node of log is introduced only when the input is a Tensor
-        self.input = input
+        super().__init__(input)
 
     def backward(self, grad):
         self.input.backward(1 / self.input.to_np() * grad)
 
 
-class Pow(Op):
+class Pow(TwoOp):
     def __init__(self, a1, a2):
-        self.a1_np, self.a2_np = (
-            i.to_np() if isinstance(i, Tensor) else i for i in [a1, a2]
-        )
-        self.a1, self.a2 = (i if isinstance(i, Tensor) else None for i in [a1, a2])
+        super().__init__(a1, a2)
 
     def backward(self, grad):
         if self.a1 is not None:
@@ -106,34 +109,43 @@ class Pow(Op):
             self.a2.backward(np.log(self.a1_np) * (self.a1_np) ** (self.a2_np) * grad)
 
 
-class ReductionWrapper(Op):
+class Sum(SingleOp):
     def __init__(self, input: Tensor, axis, keepdims):
-        self.input = input
+        super().__init__(input)
         self.axis = axis
         self.keepdims = keepdims
 
     def backward(self, grad):
-        if self.keepdims or self.axis is None:
+        if self.axis is None or self.keepdims:
             self.input.backward(grad)
         else:
             self.input.backward(np.expand_dims(grad, self.axis))
 
 
-# We don't need Sum node since it is equivalent to a ReductionWrapper node
-
-
-class Abs(Op):
+class Abs(SingleOp):
     def __init__(self, input: Tensor):
-        self.input = input
+        super().__init__(input)
 
     def backward(self, grad):
         self.input.backward(np.where(grad < 0, -grad, grad))
 
 
-class Mask(Op):
-    def __init__(self, input: Tensor, mask):
-        self.input = input
-        self.mask = mask
+class MinMax(SingleOp):
+    def __init__(self, input: Tensor, indices: Tensor, axis, keepdims):
+        super().__init__(input)
+        self.indices = indices  # result of argmin / argmax
+        self.axis, self.keepdims = axis, keepdims
 
     def backward(self, grad):
-        self.input.backward(np.where(self.mask, grad, 0.0))
+        if not self.keepdims:
+            tile_reps = [1] * self.input.ndim
+            tile_reps[self.axis] = self.input.shape[self.axis]
+            grad = np.expand_dims(grad, axis=self.axis)
+            grad = np.tile(grad, tile_reps)
+        baseline_expansion_axis = list(range(self.input.ndim))
+        baseline_expansion_axis.pop(self.axis)
+        baseline_indices = np.expand_dims(
+            np.arange(self.input.shape[self.axis]), axis=baseline_expansion_axis
+        )
+        mask = self.indices == baseline_indices
+        self.input.backward(np.where(mask, grad, 0.0))

src/deep/op.py
@@ -27,19 +27,15 @@ def max(input: Tensor, axis=None, keepdims=False):
         np.max(input.to_np(), axis=axis, keepdims=keepdims), requires_grad=False
     )
     if input.requires_grad:
+        # FIXME: wrap the following two lines to ensure safety modification of requires_grad
         res.requires_grad = True
+        res.grad = np.zeros(res.shape)
         if axis == None:
             # TODO: distribute gradient flow evenly
             pass
         else:
             indices = np.argmax(input.to_np(), axis=axis, keepdims=True)
-            baseline_expansion_axis = list(range(input.ndim))
-            baseline_expansion_axis.pop(axis)
-            baseline_indices = np.expand_dims(
-                np.arange(input.shape[axis]), axis=baseline_expansion_axis
-            )
-            mask = indices == baseline_indices
-            res.dep = ReductionWrapper(Mask(input, mask), axis, keepdims)
+            res.dep = MinMax(input, indices, axis, keepdims)
     return res
 
 
@@ -48,19 +44,15 @@ def min(input: Tensor, axis=None, keepdims=False):
         np.min(input.to_np(), axis=axis, keepdims=keepdims), requires_grad=False
     )
     if input.requires_grad:
+        # FIXME: wrap the following two lines to ensure safety modification of requires_grad
         res.requires_grad = True
+        res.grad = np.zeros(res.shape)
         if axis == None:
             # TODO: distribute gradient flow evenly
             pass
         else:
             indices = np.argmin(input.to_np(), axis=axis, keepdims=True)
-            baseline_expansion_axis = list(range(input.ndim))
-            baseline_expansion_axis.pop(axis)
-            baseline_indices = np.expand_dims(
-                np.arange(input.shape[axis]), axis=baseline_expansion_axis
-            )
-            mask = indices == baseline_indices
-            res.dep = ReductionWrapper(Mask(input, mask), axis, keepdims)
+            res.dep = MinMax(input, indices, axis, keepdims)
     return res
 
 
@@ -80,9 +72,9 @@ def argmin(input: Tensor, axis=-1, keepdims=False):
 
 def softmax(input: Tensor, axis=-1):
     input = exp(input - max(input, axis=axis, keepdims=True))
-    return input / sum(input, axis=axis)
+    return input / sum(input, axis=axis, keepdims=True)
 
 
 def log_softmax(input: Tensor, axis=-1):
     input = input - max(input, axis=axis, keepdims=True)
-    return input - log(sum(exp(input), axis=axis))
+    return input - log(sum(exp(input), axis=axis, keepdims=True))

src/deep/tensor.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .utils import *
+
 
 class Tensor(np.ndarray):
     def __new__(
@@ -21,7 +23,11 @@ class Tensor(np.ndarray):
             obj = super().__new__(subtype, arg0, dtype, buffer, offset, strides, order)
 
         obj.requires_grad, obj.dep = requires_grad, dep
-        obj.grad = None
+        if requires_grad:
+            obj.grad = np.zeros(obj.shape)
+        else:
+            obj.grad = None
+        obj.depended_count = [0]  # for topo sorting in backward
         return obj
 
     def __array_finalize__(self, obj):
@@ -29,6 +35,8 @@ class Tensor(np.ndarray):
             return
         self.requires_grad = getattr(obj, "requires_grad", False)
         self.dep = getattr(obj, "dep", None)
+        self.depended_count = getattr(obj, "depended_cound", [0])
+        self.grad = getattr(obj, "grad", None)
 
     def to_np(self):
         return self.view(np.ndarray)
@@ -37,15 +45,15 @@ class Tensor(np.ndarray):
         if not self.requires_grad:
             return
         if self.grad is None:
-            self.grad = np.zeros(self.shape)
-        if grad is None:
+            raise RuntimeError("Gradient not initialized")
+        if grad is None:  # source point of compute graph
             # TODO: support Vector-Jacobian Product like pytorch
             assert self.size == 1  # must be scalar
             self.grad += 1
         # otherwise receive gradient from graph
         elif grad.size == 1:
             self.grad += np.tile(grad, self.shape)
-        elif grad.shape == self.shape:
+        elif is_broadcastable(grad.shape, self.shape):
             self.grad += grad
         elif grad.shape != self.shape:
             grad_ = grad
@@ -53,7 +61,12 @@ class Tensor(np.ndarray):
             for dim, keepdims in dim_to_reduce:
                 grad_ = grad_.sum(axis=dim, keepdims=keepdims)
             self.grad += grad_
-        if self.dep is not None:
+        else:
+            raise ValueError(
+                f"Invalid gradient shape. Expected {self.shape}, but got {grad.shape}"
+            )
+        self.depended_count[0] -= 1
+        if self.dep is not None and self.depended_count[0] <= 0:
             self.dep.backward(self.grad)  # trigger graph
 
     # override operations
@@ -81,7 +94,7 @@ class Tensor(np.ndarray):
             elif method == "reduce":
                 axis = getattr(kwargs, "axis", None)
                 keepdims = getattr(kwargs, "keepdims", False)
-                res = Tensor(result_np, dep=ReductionWrapper(*inputs, axis, keepdims))
+                res = Tensor(result_np, dep=Sum(*inputs, axis, keepdims))
             else:
                 return NotImplemented
         elif ufunc is np.subtract:

tests/backward_test.py
@@ -174,11 +174,11 @@ def test_pow():
 def test_gradient_from_multiple_paths():
     input_np = np.random.rand(10)
     input = Tensor(input_np)
-    res = sum(input + input * input)
+    res = sum(input + input * input / exp(input))
     res.backward()
 
-    func = lambda x: sum(x + x * x, axis=-1)
-    assert_close(numerical_grad(func, input), input.grad)
+    func = lambda x: sum(x + x * x / exp(x), axis=-1)
+    assert_close(numerical_grad(func, input).to_np(), input.grad)
 
 
 def test_max():
@@ -188,7 +188,7 @@ def test_max():
     res.backward()
 
     func = lambda x: sum(max(x, axis=-1), axis=-1)
-    assert_close(numerical_grad(func, input), input.grad)
+    assert_close(numerical_grad(func, input).to_np(), input.grad)
 
 
 def test_min():
@@ -198,11 +198,30 @@ def test_min():
     res.backward()
 
     func = lambda x: sum(min(x, axis=-1), axis=-1)
-    assert_close(numerical_grad(func, input), input.grad)
+    assert_close(numerical_grad(func, input).to_np(), input.grad)
 
 
-"""
-def test_composition():
-    return True
-    
-"""
+def test_softmax():
+    input_np = np.random.rand(3, 4)
+    weight_np = np.random.rand(3, 4)
+    input = Tensor(input_np)
+    weight = Tensor(weight_np)
+    res = sum(
+        softmax(input, axis=-1) * weight
+    )  # must multiply a weight so that res != 3
+    res.backward()
+
+    func1 = lambda x: sum(sum(softmax(x, axis=-1) * weight, axis=-1), axis=-1)
+    assert_close(numerical_grad(func1, input).to_np(), input.grad)
+    func2 = lambda x: sum(sum(softmax(input, axis=-1) * x, axis=-1), axis=-1)
+    assert_close(numerical_grad(func2, weight).to_np(), weight.grad)
+
+
+def test_log_softmax():
+    input_np = np.random.rand(3, 4)
+    input = Tensor(input_np)
+    res = sum(log_softmax(input, axis=-1))
+    res.backward()
+
+    func = lambda x: sum(sum(log_softmax(x, axis=-1), axis=-1), axis=-1)
+    assert_close(numerical_grad(func, input).to_np(), input.grad)

tests/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 from deep import *
 
 
-def assert_close(actual, expected, rtol=1e-05, atol=1e-08):
+def assert_close(expected, actual, rtol=5e-05, atol=1e-08):
     delta = np.abs(actual - expected)
     tol = atol + rtol * np.abs(expected)
     if np.isscalar(delta):