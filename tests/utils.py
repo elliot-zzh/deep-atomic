30e60391be5b3adf43be863f6edaeefd206dbb94
elliot-zzh <elliot_zh@proton.me>
2026-07-13 14:01:40 +0800

    refactor: rename input_ to x in tests/utils.py numerical_grad

--- tests/utils.py
+++ tests/utils.py
@@ -12,24 +12,24 @@ def assert_close(expected, actual, rtol=5e-05, atol=1e-08):
         assert (delta <= tol).all()
 
 
-# TODO: more to implement on this utility. handle multi-input func, handle vector-valued output
-def numerical_grad(func, input_, eps=1e-5):
-    original_shape = input_.shape
-    input_ = input_.reshape(-1)
+# TODO: more to implement on this utility. handle multi-argument func, handle vector-valued output
+def numerical_grad(func, x, eps=1e-5):
+    original_shape = x.shape
+    x = x.reshape(-1)
 
-    X_plus = np.tile(input_, (input_.size, 1))  # Copy x0 into n rows
+    X_plus = np.tile(x, (x.size, 1))  # Copy x0 into n rows
     np.fill_diagonal(X_plus, X_plus.diagonal() + eps)
-    X_plus = X_plus.reshape([input_.size] + list(original_shape))
+    X_plus = X_plus.reshape([x.size] + list(original_shape))
 
-    X_minus = np.tile(input_, (input_.size, 1))
+    X_minus = np.tile(x, (x.size, 1))
     np.fill_diagonal(X_minus, X_minus.diagonal() - eps)
-    X_minus = X_minus.reshape([input_.size] + list(original_shape))
+    X_minus = X_minus.reshape([x.size] + list(original_shape))
 
-    res = np.zeros([input_.size])
+    res = np.zeros([x.size])
     # use a for loop to iterate through all parameters
     # slower but ensure correct and more convenient operations in func
     # e.g. .sum(axis=None) appeared in func will cause reduction of the added parameter dim, which is undesired
-    for i in range(input_.size):
+    for i in range(x.size):
         # central difference
         res[i] = (func(X_plus[i]) - func(X_minus[i])) / (eps * 2)
 
